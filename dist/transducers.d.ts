1896e46229af572c675439b11527de8c98a61f4c
zdrr <zdravko.tatarski@gmail.com>
2019-07-14 07:30:01 +0300

    chore: cleanup old action interface

--- dist/transducers.d.ts
+++ dist/transducers.d.ts
@@ -1,4 +1,5 @@
-import { ReducerFn, IAction } from './interfaces';
+import { ReducerFn } from './interfaces';
+import { Action } from 'ts-action';
 /**
  *
  * @param mapFn - a function to map a state with
@@ -7,14 +8,14 @@ import { ReducerFn, IAction } from './interfaces';
  * PS - previous state
  * NS - next state
  */
-export declare const mapPS: <State, A extends IAction>(mapFn: (state: State) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
-export declare const mapNS: <State, A extends IAction>(mapFn: (state: State) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const mapPS: <State, A extends Action<string>>(mapFn: (state: State) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const mapNS: <State, A extends Action<string>>(mapFn: (state: State) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
 /**
  *
  * @param mapFn - a function to map an action with
  * @returns {TransducerFn} TransducerFn<State, ActionsUnion>
  */
-export declare const mapA: <State, A extends IAction>(mapFn: (action: A) => A) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const mapA: <State, A extends Action<string>>(mapFn: (action: A) => A) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
 /**
  *
  * @param filterFn - a function to filter a state with
@@ -23,14 +24,14 @@ export declare const mapA: <State, A extends IAction>(mapFn: (action: A) => A) =
  * PS - previous state
  * NS - next state
  */
-export declare const filterPS: <State, A extends IAction>(filterFn: (state: State) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
-export declare const filterNS: <State, A extends IAction>(filterFn: (state: State) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const filterPS: <State, A extends Action<string>>(filterFn: (state: State) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const filterNS: <State, A extends Action<string>>(filterFn: (state: State) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
 /**
  *
  * @param filterFn - a function to filter an action with
  * @returns {TransducerFn} TransducerFn<State, ActionsUnion>
  */
-export declare const filterA: <State, A extends IAction>(filterFn: (action: A) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const filterA: <State, A extends Action<string>>(filterFn: (action: A) => boolean) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
 /**
  * Reduce into state
  * @param reduceFn - a function to reduce the state and action together
@@ -39,12 +40,12 @@ export declare const filterA: <State, A extends IAction>(filterFn: (action: A) =
  * PS - previous state
  * NS - next state
  */
-export declare const reducePS: <State, A extends IAction>(reducerFn: (state: State, action: A) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
-export declare const reduceNS: <State, A extends IAction>(reducerFn: (state: State, action: A) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const reducePS: <State, A extends Action<string>>(reducerFn: (state: State, action: A) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const reduceNS: <State, A extends Action<string>>(reducerFn: (state: State, action: A) => State) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
 /**
  *
  * Reduce into action
  * @param reduceFn - a function to reduce the state and action together
  * @returns {TransducerFn} TransducerFn<State, ActionsUnion>
  */
-export declare const reduceA: <State, A extends IAction>(reducerFn: (state: State, action: A) => A) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
+export declare const reduceA: <State, A extends Action<string>>(reducerFn: (state: State, action: A) => A) => (reducer: ReducerFn<State, A>) => (state: State, action: A) => State;
