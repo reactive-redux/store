754a77fe7386bb0ca9206cd0f01c54f8f69b16d6
Zdravko Tatarski <zdravko.tatarski@gmail.com>
2020-04-05 02:51:45 +0300

    chore: remove dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { AsyncType, FlattenOperator, ReducerFn } from './interfaces';
-export { Store, createStore } from './store';
-export { mapToObservable, catchErr } from './utils';
-export * from './middleware';
-export * from 'ts-action';
-export * from 'reselect';
--- dist/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { Observable, OperatorFunction } from 'rxjs';
-import { AsyncType, IAction } from './interfaces';
-export declare const isObject: (value: any) => boolean;
-export declare const hasType: (action: any) => boolean;
-export declare const compose: (fns: any[]) => any;
-export declare const catchErr: import("rxjs").UnaryFunction<Observable<unknown>, Observable<any>>;
-export declare const flatCatch: <T>(o: Observable<T>) => Observable<T>;
-export declare const mapToObservable: <T>(value: AsyncType<T>) => Observable<T>;
-export declare function ofType<T extends IAction>(...allowedTypes: string[]): OperatorFunction<IAction, T>;
